85dc9b4eb41adee7dfd123ed986ffee01723450f
awais-urrehman <awais.jarral@issm.ai>
2025-02-24 19:30:27 +0500

    updated: cors

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ const port = process.env.PORT || 4000;
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-app.use(cors());
-
 app.use(
   cors({
     origin: true,
