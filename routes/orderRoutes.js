44d995c63d9652f2903184b5d5a8df9fecb7007b
awais-urrehman <awais.jarral@issm.ai>
2025-03-09 20:55:35 +0500

    fixed:

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,8 +7,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
+    user: "mahnoor@farhatrena.com",
+    pass: "xtly tocc qvel hckk",
   },
 });
 
